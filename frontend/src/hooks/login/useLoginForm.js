679b5849b73b19af9dc76c51b507640d0a66db23
Dionathan Boing Mesquita <dionathandev@gmail.com>
2026-04-08 21:57:38 -0300

    feat: add request login with axios

--- frontend/src/hooks/login/useLoginForm.js
+++ frontend/src/hooks/login/useLoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 function useLoginForm() {
   const [form, setForm] = useState({
@@ -13,8 +14,21 @@ function useLoginForm() {
     });
   }
 
-  function submit(e) {
-    return e.preventDefault();
+  async function submit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://localhost:3000/api/v1/auth/login",
+        data: {
+          ...form,
+        },
+      });
+      console.log("Dados enviados com sucesso:", response.data);
+      alert("Formulário enviado!");
+    } catch (error) {
+      console.error("Motivo da recusa do servidor:", error.response?.data);
+    }
   }
 
   return {
